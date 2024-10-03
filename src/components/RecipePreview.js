0555f66d50be546ad81a17311ae568c1d4327ace
Chris Fitzmorris <cfitz93@gmail.com>
2024-10-03 14:48:32 -0500

    Truncate recipe description on preview, make buttons functional

--- src/components/RecipePreview.js
+++ src/components/RecipePreview.js
@@ -9,8 +9,14 @@ import {
 } from "@mui/material";
 import Link from "next/link";
 
+const truncateText = (text, maxLength) => {
+  return text.length > maxLength ? text.substr(0, maxLength - 1) + "..." : text;
+};
+
+const DESCRIPTION_LENGTH = 300;
+
 export default function RecipePreview(props) {
-  const { id, title, image, description } = props.recipe;
+  const { id, title, image, description, url } = props.recipe;
   return (
     <Card>
       <CardActionArea component={Link} href={`/recipe/${id}`}>
@@ -23,14 +29,27 @@ export default function RecipePreview(props) {
           <Typography gutterBottom variant="h5">
             {title}
           </Typography>
-          <Typography variant="body2">{description}</Typography>
+          <Typography variant="body2">
+            {truncateText(description, DESCRIPTION_LENGTH)}
+          </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="sm" color="primary">
+        <Button
+          size="sm"
+          color="primary"
+          component={Link}
+          href={`/recipe/${id}`}
+        >
           Read
         </Button>
-        <Button size="sm" color="primary">
+        <Button
+          size="sm"
+          color="primary"
+          component={Link}
+          href={url}
+          target="_blank"
+        >
           Visit original site
         </Button>
       </CardActions>
